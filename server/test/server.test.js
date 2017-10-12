65ae9f87ac9d7ad29d8e91ae7a2d17dcaf99fb9a
Redmonty <putiareddevil@gmail.com>
2017-10-12 17:26:56 +0300

    Add tests for GET/todos/:id

--- server/test/server.test.js
+++ server/test/server.test.js
@@ -1,13 +1,16 @@
 const expect = require('expect'),
-    request = require('supertest');
+    request = require('supertest'),
+    {ObjectID} = require('mongodb');
 const {app} = require('./../server'),
     {Todo} = require('./../models/todo');
 
 const todos = [
     {
+        _id: new ObjectID(),
         text: 'First text todo'
     },
     {
+        _id: new ObjectID(),
         text: 'Second text todo'
     }
 ];
@@ -74,6 +77,30 @@ describe('GET /todos', () => {
             .expect((res) => {
                 expect(res.body.todos.length).toBe(2);
             })
+            .end(done);
+    });
+});
+
+describe('GET /todos/:id', () => {
+    it('should return todo doc', (done) => {
+        request(app)
+            .get(`/todos/${todos[0]._id.toHexString()}`)
+            .expect(200)
+            .expect((res) => {
+                expect(res.body.todo.text).toBe(todos[0].text);
+            })
+            .end(done);
+    });
+    it('should return 404 if todo not found', (done) => {
+        request(app)
+            .get(`/todos/${new ObjectID().toHexString()}`)
+            .expect(404)
+            .end(done);
+    });
+    it('should return 404 for non object ids', (done) => {
+        request(app)
+            .get(`/todos/123abs`)
+            .expect(404)
             .end(done);
     });
 });
