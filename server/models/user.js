497847a389d47031f73b02f1861bf7dff75de9c7
Redmonty <putiareddevil@gmail.com>
2017-10-14 12:42:10 +0300

    Create generateAuthToken for User model

--- server/models/user.js
+++ server/models/user.js
@@ -1,7 +1,9 @@
 const mongoose = require('mongoose'),
-    validator = require('validator');
+    validator = require('validator'),
+    jwt = require('jsonwebtoken'),
+    _ = require('lodash');
 
-const User = mongoose.model('User', {
+const UserSchema = new mongoose.Schema({
     email: {
         type: String,
         trim: true,
@@ -31,6 +33,25 @@ const User = mongoose.model('User', {
         }
     }]
 });
+UserSchema.methods.toJSON = function() {
+    var user = this;
+    var userObject = user.toObject();
+
+    return _.pick(userObject, ['_id', 'email'])
+};
+UserSchema.methods.generateAuthToken = function() {
+    var user = this;
+    var access = 'auth';
+    var token = jwt.sign({_id: user._id.toHexString(), access: access}, 'abc123').toString();
+    user.tokens.push({access, token});
+
+    return user.save().then(() => {
+        return token;
+    });
+};
+const User = mongoose.model('User', UserSchema);
+
+
 
 module.exports = {
     User
--- server/server.js
+++ server/server.js
@@ -84,7 +84,15 @@ app.patch('/todos/:id', (req,res) => {
 
 app.post('/users', (req, res) => {
     var body = _.pick(req.body, ['email','password']);
-    var user = new User(body).save().then((user) => {res.send(user)}, (err) => {res.status(400).send(err);});
+    var user = new User(body);
+    user.save().then(() => {
+        return user.generateAuthToken();
+        // res.send(user);
+    }).then((token) => {
+        res.header('x-auth', token).send(user);
+    }).catch((err) => {
+        res.status(400).send(err);
+    });
 });
 /*
 const ivan = new User({
