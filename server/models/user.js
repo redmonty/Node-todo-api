10657efe4b2035e74772dc26d577a153df1816a2
Redmonty <putiareddevil@gmail.com>
2017-10-15 12:52:58 +0300

    hash user password before save

--- server/models/user.js
+++ server/models/user.js
@@ -1,7 +1,8 @@
 const mongoose = require('mongoose'),
     validator = require('validator'),
     jwt = require('jsonwebtoken'),
-    _ = require('lodash');
+    _ = require('lodash'),
+    bcrypt = require('bcryptjs');
 
 const UserSchema = new mongoose.Schema({
     email: {
@@ -70,6 +71,22 @@ UserSchema.statics.findByToken = function(token) {
         res.status(401).send();
     });
 };
+UserSchema.pre('save', function(next) {//middlewere - run code before save user
+    var user = this;
+    if(user.isModified('password')) {
+        bcrypt.genSalt(10, (err, salt) => {
+            bcrypt.hash(user.password, salt, (err, hash) => {
+                user.password = hash;
+                next();
+            });
+        });
+        // bcrypt.compare(hashPassword, user.password, (err, res) => {
+        //     next();
+        // }); 
+    } else {
+        next();
+    }
+});
 const User = mongoose.model('User', UserSchema);
 
 
